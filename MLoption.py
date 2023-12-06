eba5497358d4f29c40a1b86dca36c4c61abec3c7
RahulArora0603 <rahul.arora0603@gmail.com>
2023-12-06 20:33:20 +0530

    abc

---
+++ MLoption.py
@@ -0,0 +1,6 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression , LogisticRegression
+
+models = ["LinearRegression" , "LogisticRegression" , "KNearestNeighbours" , "Decision Tree","RandomForestClassifier", "SupportVectorMachine", "NaiveByes"]
+model_name = input("Enter model name: ")
+
--- main_project.py
+++ main_project.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression , LogisticRegression
 #Takes CSV as input -
 csvname = input("Enter csv path: ")
 csvname = list(csvname)
@@ -55,7 +56,7 @@ def view_data():
             print("No such column found")
 
 def data_Visualisation():
-    graphtype = input("Press A for Line Chart , Press B for Bar Graph , Press C for Pie Chart, Press D for Histogram , Press E for Scatter Plot, Press F for Area Chart, Press G for Main Menu, Press H to exit \n")
+    graphtype = input("Press A for Line Chart , Press B for Bar Graph , Press C for Pie Chart, Press D for Histogram , Press E for Scatter Plot, Press G for Main Menu, Press H to exit \n")
     grp_options = ["A","B","C","D","E","F", "G","H"]
     def line_Chart():
         col_name1 = input("Enter first column(numerical values): \n")
@@ -88,4 +89,95 @@ def data_Visualisation():
 
     def histoGram():
         print("Warning : Use histogram only when the data is continuous. Example : Marks of Students")
-        x = 
+        col_name = input("Enter column : ")
+        no_bins = int(input("Number of bins : "))
+        x = df[col_name].values
+        plt.hist(x ,bins=no_bins)
+        plt.show()
+
+    def scatter_Plot():
+        sc_col1 = input("Enter x-axis column: ")
+        sc_col2 = input("Enter y-axis column: ")
+        sc_color = input("Enter color of points: ")
+        x = df[sc_col1]
+        y = df[sc_col2]
+        plt.scatter(x, y , c=sc_color)
+        plt.xlabel(sc_col1)
+        plt.ylabel(sc_col2)
+        plt.show()
+
+def mach_Learn():
+    modtype = input("Press A for KNNeighbours Prediction , Press B for Random Forest Classifier, Press C for Decision Tree, Press 'D' for Linear Regression , 'E' for Logistic Regression, 'F' to return to main menu : ")
+    if modtype=='D': #Linear Regression
+       para1 = input("Enter independent variables : ")
+       para2 = input("Enter dependent variables : ")
+       comma = ','
+       if comma in list(para1):
+          para1 = para1.split(',')
+          x = df[para1].values
+          y = df[para2].values
+       else:
+          x = df[para1].values
+          y = df[para2].values
+          x = x.reshape(-1 , 1)
+       X_train , X_test , Y_train , Y_test = train_test_split(x , y , test_size=1/4 , random_state=0)  
+       model = LinearRegression()
+       model.fit(X_train, Y_train)
+       ypred = model.predict(X_test)
+       print(ypred)
+       to_predict = input("Enter values to predict: ")
+       if comma in list(to_predict):
+         to_predict=to_predict.split(",")
+         ipredict = []
+         for i in range(0, len(para1)):
+            if str(df[para1[i]].dtype).startswith("int")==True:
+                r = int(to_predict[i])
+                ipredict.append(r)
+            elif str(df[para1[i]].dtype).startswith("float")==True:
+                r = float(to_predict[i])
+                ipredict.append(r)
+         print(ipredict)         
+         prediction = model.predict([ipredict])
+         print(prediction)  
+       else:
+         mypred = int(to_predict)
+         prediction = model.predict([[mypred]])
+         print(prediction)
+    elif modtype=='E': #Logistic Regression   
+       para1 = input("Enter independent variables : ")
+       para2 = input("Enter dependent variables : ")
+       comma = ','
+       if comma in list(para1):
+          para1 = para1.split(',')
+          x = df[para1].values
+          y = df[para2].values
+       else:
+          x = df[para1].values
+          y = df[para2].values
+          x = x.reshape(-1 , 1)
+       X_train , X_test , Y_train , Y_test = train_test_split(x , y , test_size=1/4 , random_state=0)  
+       model = LogisticRegression()
+       model.fit(X_train, Y_train)
+       ypred = model.predict(X_test)
+       print(ypred)
+       to_predict = input("Enter values to predict: ")
+       if comma in list(to_predict):
+         to_predict=to_predict.split(",")
+         ipredict = []
+         for i in range(0, len(para1)):
+            if str(df[para1[i]].dtype).startswith("int")==True:
+                r = int(to_predict[i])
+                ipredict.append(r)
+            elif str(df[para1[i]].dtype).startswith("float")==True:
+                r = float(to_predict[i])
+                ipredict.append(r)
+         print(ipredict)         
+         prediction = model.predict([ipredict])
+         print(prediction)
+       else:
+         mypred = int(to_predict)
+         prediction = model.predict([[mypred]])
+         print(prediction)
+
+def checkDataType(col_list):
+    df[]
